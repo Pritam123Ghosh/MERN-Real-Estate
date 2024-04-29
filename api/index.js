4de8d7e2a85e105fa0d40fc36d7e047b8d9c12e4
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-04-29 20:05:10 +0530

    Created sign in API route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import  userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 
 dotenv.config();
-(async() =>{
+(async () => {
     try {
         await mongoose.connect(`${process.env.MONGO}`)
         console.log("Database connected")
@@ -18,14 +18,14 @@ const app = express();
 
 app.use(express.json())
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
     return res.status(statusCode).json({
@@ -34,3 +34,6 @@ app.use((err, req, res, next)=>{
         message
     })
 })
+
+
+
