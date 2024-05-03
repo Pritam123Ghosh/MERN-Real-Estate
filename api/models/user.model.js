f0ebe9273d0f693481b658e72b2c5e01f7979bbf
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-05-03 09:57:46 +0530

    Fixed bugs in avatar model and Completed image upload functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -13,7 +13,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRf-DqKr-z7qDHe0IOBDypqLB9hF0QHF5azH59HJTxZg&s"
+    },
 }, { timestamps: true })
 
 const User = mongoose.model('User', userSchema);
