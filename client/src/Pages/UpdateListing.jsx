ea7f7b3f95a1e153de04a90e8679d7c5105cd122
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-05-19 09:01:11 +0530

    Create the listing item comp and show listing

--- client/src/Pages/UpdateListing.jsx
+++ client/src/Pages/UpdateListing.jsx
@@ -368,7 +368,7 @@ useEffect(()=>{
               </div>
             ))}
           <button disabled={loading || uploading} className="p-3 bg-green-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            {loading ? "Creating..." : "Update listing"}
+            {loading ? "Updating..." : "Update listing"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
